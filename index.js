a643a4794a1197dc5c1d91e2c16b4593e3498230
th.hoang04 <140410385+BuiThienHoang221204@users.noreply.github.com>
2024-02-09 18:49:14 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var w = c.width = window.innerWidth,
 		opts = {
 			strings: [ 'HAPPY NEW YEAR!','CHÚC YOU NĂM MỚI DUI DẺ','NHỚ LÌ XÌ CHO TOI ^-^'],
 			charSize: 25,
-			charSpacing: 20,
+			charSpacing: 25,
 			lineHeight: 50,
 			
 			cx: w / 2,
